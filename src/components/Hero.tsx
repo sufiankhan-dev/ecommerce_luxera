0f2c7a0917d96e4415223dbd32a501300570725f
M Sufian Khan <sufiancoding@gmail.com>
2024-07-12 22:40:39 +0500

    hero update

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -84,7 +84,7 @@ const Hero = () => {
         <div>
           <img src="slide-1.png" />
           <div className="px-[15px] md:px-[40px] py-[10px] md:py-[25px] font-oswald bg-white absolute bottom-[25px] md:bottom-[75px] left-0 text-black/[0.9] text-[15px] md:text-[30px] uppercase font-medium cursor-pointer hover:opacity-90">
-            Shop now
+            Shop now here
           </div>
         </div>
         <div>
