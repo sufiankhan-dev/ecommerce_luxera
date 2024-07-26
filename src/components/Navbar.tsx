f0abf3d85bdbf2b5a06ef59ccb18e4c67151414a
M Sufian Khan <sufiancoding@gmail.com>
2024-07-26 17:28:09 +0500

    cart adjustments

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -77,7 +77,7 @@ const Navbar = ({ Mensdata, Womensdata }: CategoriesItems) => {
           {!session && (
             <div
               onClick={() => signIn()}
-              className="relative hidden md:flex uppercase text-lg gap-x-2 rounded-md mx-3 font-normal mt-1 tracking-wider hover:text-black cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-500 before:absolute before:bg-black before:origin-center before:h-[1.5px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-500 after:absolute after:bg-black after:origin-center after:h-[1.5px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]"
+              className="relative hidden md:flex uppercase text-lg gap-x-2 rounded-md mx-3 font-medium mt-1 tracking-wide hover:text-black cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-500 before:absolute before:bg-black before:origin-center before:h-[1.5px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-500 after:absolute after:bg-black after:origin-center after:h-[1.5px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]"
             >
               <p>Login</p>
             </div>
--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -64,7 +64,7 @@ const ShoppingCart = () => {
         <SheetHeader>
           <SheetTitle className="uppercase">Your Cart</SheetTitle>
         </SheetHeader>
-        <div className="h-full flex flex-col justify-between">
+        <div className="h-full flex flex-col justify-between -mx-7">
           <div className="mt-8 flex-1 overflow-y-auto">
             <ul className="-my-6 divide-y divide-gray-200">
               {cartCount === 0 ? (
@@ -79,7 +79,7 @@ const ShoppingCart = () => {
               ) : (
                 <>
                   {Object.values(cartDetails ?? {}).map((entry) => (
-                    <li key={entry.id} className="flex py-6">
+                    <li key={entry.id} className="flex py-6 px-4 md:px-7">
                       <div className="h-24 w-24 flex-shrink-0 overflow-hidden border border-gray-200">
                         <Image
                           src={entry.image as string}
